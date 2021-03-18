e47456f31f8d1dcf3768318197e8a0e08239c315
Huixies <1062240360@qq.com>
2021-03-19 06:18:12 +0800

    多级路由完成

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <Header />
+    </div>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name:"app",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/about/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="row mb-5">
+      <div class="col-4">
+        <!-- 导航 -->
+        <div class="list-group mb-5">
+          <div class="nav-link">
+            <router-link class="list-group-item list-group-item-action" to="/about/history">
+              历史订单
+            </router-link>
+          </div>
+          <div class="nav-link">
+            <router-link class="list-group-item list-group-item-action" to="/about/contact">
+              联系我们
+            </router-link>
+          </div>
+          <div class="nav-link">
+            <router-link class="list-group-item list-group-item-action" to="/about/delivery">
+              点餐文档
+            </router-link>
+          </div>
+          <div class="nav-link">
+            <router-link class="list-group-item list-group-item-action" to="/about/orderinguide">
+              快递信息
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-8">
+        <!-- 导航所对应的内容 -->
+        <router-view></router-view>
+      </div>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
